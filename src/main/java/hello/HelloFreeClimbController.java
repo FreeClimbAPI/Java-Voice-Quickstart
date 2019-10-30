bb44d17fb1f602131e3c60521894f98dd405e87e
Katherine O'Hearn <kohearn@vailsys.com>
2019-10-30 15:23:48 -0500

    Update name to FreeClimb

--- src/main/java/hello/HelloFreeClimbController.java
+++ src/main/java/hello/HelloFreeClimbController.java
@@ -3,11 +3,11 @@ package main.java.hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.vailsys.persephony.percl.PerCLScript;
-import com.vailsys.persephony.percl.Say;
+import com.vailsys.freeclimb.percl.PerCLScript;
+import com.vailsys.freeclimb.percl.Say;
 
 @RestController
-public class HelloPersephonyController {
+public class HelloFreeClimbController {
   @RequestMapping("/voice")
   public String index() {
     PerCLScript script = new PerCLScript();
