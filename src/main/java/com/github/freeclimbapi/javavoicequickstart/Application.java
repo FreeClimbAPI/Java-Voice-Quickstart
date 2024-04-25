57b730b31488c379a341d9f5c31bcc0f25d72480
Bhargav Ganesh <bganesh@vailsys.com>
2024-04-25 10:40:20 -0500

    Added dockerfile, actions and other minor changes

--- src/main/java/com/github/freeclimbapi/javavoicequickstart/Application.java
+++ src/main/java/com/github/freeclimbapi/javavoicequickstart/Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class JavaVoiceQuickstartApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaVoiceQuickstartApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
